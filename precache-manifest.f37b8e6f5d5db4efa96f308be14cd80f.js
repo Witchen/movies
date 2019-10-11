3a0ef6e93074ef790b73a067443d15d3e0fde1ec
Witchen <witchenhendry@yahoo.co.id>
2019-10-12 00:24:27 +0800

    Updates

---
+++ precache-manifest.f37b8e6f5d5db4efa96f308be14cd80f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f5b8430f3cfa5881d347efa592cbe9",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "4765f7c30f427682cf0b",
+    "url": "/movies/static/css/main.de566a39.chunk.css"
+  },
+  {
+    "revision": "41483802e479f1e1cb87",
+    "url": "/movies/static/js/2.36658c39.chunk.js"
+  },
+  {
+    "revision": "4765f7c30f427682cf0b",
+    "url": "/movies/static/js/main.6a349d08.chunk.js"
+  },
+  {
+    "revision": "78dbb36ae65c53bcc8f1",
+    "url": "/movies/static/js/runtime-main.83833e2a.js"
+  }
+]);
